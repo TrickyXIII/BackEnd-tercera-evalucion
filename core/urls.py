bc4bc787974900f22415cc4bcd5fc57b3da037cd
TrickyXIII <martin.henriquez.bacian@gmail.com>
2025-12-14 00:32:50 -0300

    Se agregaron rutas de API y reporte

--- core/urls.py
+++ core/urls.py
@@ -1,24 +1,13 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from tienda import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'insumos', views.InsumoViewSet)
+router.register(r'pedidos', views.PedidoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +17,7 @@ urlpatterns = [
     path('pedido/<int:producto_id>/', views.crear_pedido, name='crear_pedido'),
     path('seguimiento/', views.seguimiento, name='seguimiento_home'),
     path('seguimiento/<uuid:token>/', views.seguimiento, name='seguimiento_url'),
+    path('api/', include(router.urls)),
+    path('api/pedidos/filtrar/', views.filtrar_pedidos),
+    path('reporte/', views.reporte, name='reporte'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
